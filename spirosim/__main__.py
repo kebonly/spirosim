35e9b66960979dbcbed9fe1b8b5c1726cfd2b323
kebonly <kevin.sly99@gmail.com>
2024-11-16 19:48:49 -0800

    simple viscek model

---
+++ spirosim/__main__.py
@@ -0,0 +1,4 @@
+if __name__ == "__main__":
+    # Think of __main__.py like running a binary...
+    # This isn't really relevant to this project since we're just running a simulation
+    # using a Jupyter notebook
